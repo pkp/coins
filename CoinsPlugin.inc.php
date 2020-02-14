b34f1d8bf05cd1f61cdcfd93ba9e6666adb45896
Alec Smecher <alec@smecher.bc.ca>
2020-02-14 13:19:30 -0800

    Locale files to PO; copyright dates; license

--- CoinsPlugin.inc.php
+++ CoinsPlugin.inc.php
@@ -1,18 +1,15 @@
 <?php
 
 /**
- * @file plugins/generic/coins/CoinsPlugin.inc.php
+ * @file CoinsPlugin.inc.php
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * @class CoinsPlugin
- * @ingroup plugins_generic_coins
- *
  * @brief COinS plugin class
  */
-
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
 
 /**
- * @defgroup plugins_generic_coins
- */
-
-/**
- * @file plugins/generic/coins/index.php
+ * @file index.php
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
- * @ingroup plugins_generic_coins
  * @brief Wrapper for COinS plugin.
- *
  */
 
 require_once('CoinsPlugin.inc.php');
-
 return new CoinsPlugin();
 
